dcd6f893f03c2964d5acf80d7d654bc2de3cb740
Tim Wheeler <timwheeleronline@gmail.com>
2015-02-19 14:13:55 -0800

    adding MODL file

--- test/runtests.jl
+++ test/runtests.jl
@@ -23,10 +23,8 @@ function array_matches{S<:Real, T<:Real}(arr::AbstractVector{S}, arr2::AbstractV
 end
 
 
-
-
-
 include("test_categorical_discretizer.jl")
 include("test_linear_discretizer.jl")
 
 include("test_disc_uniformwidth.jl")
+include("test_disc_MODL.jl")
--- test/test_disc_uniformwidth.jl
+++ test/test_disc_uniformwidth.jl
@@ -1,3 +1,4 @@
+
 @test array_matches(binedges(DiscretizeUniformWidth(2), [1.0,2.0,3.0,4.0,5.0,6.0]), 
     [1.0, 3.5, 6.0], 1e-8)
 @test array_matches(binedges(DiscretizeUniformWidth(2), [6.0,2.0,30.0,4.0,1.0,1.0]), 
