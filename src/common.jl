ae73de9125ee7bda69fc1925d8287ea7c4ee27f6
Mykel Kochenderfer <m.kochenderfer@gmail.com>
2019-07-31 12:20:41 -0700

    refs #22

--- src/common.jl
+++ src/common.jl
@@ -38,30 +38,14 @@ end
 function get_histogram_plot_arrays(binedges::Vector{R}, counts::AbstractVector{I}) where {R<:Real, I<:Integer}
     n = length(binedges)
     n == length(counts)+1 || error("binedges must have exactly one more entry than counts!")
-
-    arr_x = Array{R}(n+1)
-    arr_x[1] = binedges[1]
-    arr_x[2:n+1] = binedges
-
-    arr_y = Array{I}(n+1)
-    arr_y[1] = zero(I)
-    arr_y[2:n] = counts
-    arr_y[end] = zero(I)
-
+    arr_x = [binedges[1]; binedges]
+    arr_y = [zero(I); counts; zero(I);
     (arr_x, arr_y)
 end
 function get_histogram_plot_arrays(binedges::Vector{R}, pdfs::AbstractVector{F}) where {R<:Real, F<:Real}
     n = length(binedges)
-    n == length(pdfs)+1 || error("binedges must have exactly one more entry than counts!")
-
-    arr_x = Array{R}(n+1)
-    arr_x[1] = binedges[1]
-    arr_x[2:n+1] = binedges
-
-    arr_y = Array{F}(n+1)
-    arr_y[1] = zero(F)
-    arr_y[2:n] = pdfs
-    arr_y[end] = zero(F)
-
+    n == length(pdfs)+1 || error("binedges must have exactly one more entry than pdfs!")
+    arr_x = [binedges[1]; binedges]
+    arr_y = [zero(I); pdfs; zero(I);
     (arr_x, arr_y)
 end
